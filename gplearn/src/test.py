d755da70dc6e536b822769bde49775a82b15e62b
ly <queeniiee02@gmail.com>
2023-07-26 17:00:42 +0800

    remove useless file

--- gplearn/src/test.py
+++
@@ -1,6 +0,0 @@
-print('    |{:^25}|{:^42}|'.format('Population Average',
-                                               'Best Individual'))
-print('-' * 4 + ' ' + '-' * 25 + ' ' + '-' * 42 + ' ' + '-' * 10)
-line_format = '{:>4} {:>8} {:>16} {:>8} {:>16} {:>16} {:>10}'
-print(line_format.format('Gen', 'Length', 'Fitness', 'Length',
-                                     'Fitness', 'OOB Fitness', 'Time Left'))
